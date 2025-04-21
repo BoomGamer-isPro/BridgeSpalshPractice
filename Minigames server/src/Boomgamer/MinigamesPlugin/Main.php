d179f461b4c0f5c85fd88d6ea182fa1d74526856
BoomGamer-isPro <boomgamerispro@gmail.com>
2025-04-21 13:58:43 +0530

    Update Main.php

--- Minigames server/src/Boomgamer/MinigamesPlugin/Main.php
+++ Minigames server/src/Boomgamer/MinigamesPlugin/Main.php
@@ -4,15 +4,30 @@ namespace Boomgamer\MinigamesPlugin;
 
 use Boomgamer\MinigamesPlugin\listener\PlayerListener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\world\Position;
 
 class Main extends PluginBase {
 
+    public Config $spawn;
+    
     public function onEnable(): void {
         $this->getLogger()->info("Plugin Enabled!");
-        $this->getServer()->getPluginManager()->registerEvents(new PlayerListener(), $this);
+        @mkdir($this->getDataFolder());
+        $this->saveResource("spawn.yml");
+        $this->spawn = new Config($this->getDataFolder() . "spawn.yml", Config::YAML);
+        
+        $this->getServer()->getPluginManager()->registerEvents(
+            new PlayerListener($this),
+            $this
+        );
     }
 
     public function onDisable(): void {
         $this->getLogger()->info("Plugin Disabled!");
+    }
+    
+    public function getSpawnData(): Config {
+        return $this->spawn;
     }
 }
