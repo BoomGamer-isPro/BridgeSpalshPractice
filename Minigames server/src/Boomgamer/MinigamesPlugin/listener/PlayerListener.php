1df1a5e9b26e793fb6acc31ed49a9f24425141ab
BoomGamer-isPro <boomgamerispro@gmail.com>
2025-04-21 13:59:24 +0530

    Update PlayerListener.php

--- Minigames server/src/Boomgamer/MinigamesPlugin/listener/PlayerListener.php
+++ Minigames server/src/Boomgamer/MinigamesPlugin/listener/PlayerListener.php
@@ -7,8 +7,14 @@ use Boomgamer\MinigamesPlugin\forms\GameSelectorForm;
 use Boomgamer\MinigamesPlugin\forms\ProfileForm;
 use Boomgamer\MinigamesPlugin\forms\SettingsForm;
 use Boomgamer\MinigamesPlugin\forms\ShopForm;
+use Boomgamer\MinigamesPlugin\scoreboard\Scoreboard;
 use pocketmine\block\VanillaBlocks;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\item\Item;
@@ -18,11 +24,47 @@ use pocketmine\item\VanillaItems;
 use pocketmine\player\GameMode;
 use pocketmine\utils\TextFormat;
 use pocketmine\world\sound\XpLevelUpSound;
+use pocketmine\player\Player;
+use pocketmine\world\Position;
+use Boomgamer\MinigamesPlugin\Main;
+use pocketmine\utils\Config;
+
 
 class PlayerListener implements Listener {
+    
+    private Main $plugin;
+    private Config $spawn;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+
+        // Load spawn config from plugin data folder
+        $this->spawn = new Config($this->plugin->getDataFolder() . "spawn.yml", Config::YAML);
+    }
 
     public function onJoin(PlayerJoinEvent $joinEvent) {
         $player = $joinEvent->getPlayer();
+         $data = $this->spawn->getAll();
+        
+       if (
+            isset($data["x"], $data["y"], $data["z"], $data["world"]) &&
+            is_numeric($data["x"]) &&
+            is_numeric($data["y"]) &&
+            is_numeric($data["z"])
+        ) {
+            $world = $this->plugin->getServer()->getWorldManager()->getWorldByName($data["world"]);
+            if ($world !== null) {
+                $position = new Position((int)$data["x"], (int)$data["y"], (int)$data["z"], $world);
+                $player->teleport($position);
+            } else {
+                $player->sendMessage("§cLobby world not loaded or doesn't exist!");
+            }
+        } else {
+            $player->sendMessage("§cInvalid spawn configuration.");
+        }
+
+
+        Scoreboard::sendLobbyScoreboard($player);
 
         $joinEvent->setJoinMessage(" ");
         $player->sendMessage(TextFormat::BOLD . TextFormat::RED . "Bridge" . TextFormat::BLUE . "Splash" . TextFormat::RESET . ": " . TextFormat::GREEN . "Welcome " . $player->getName());
@@ -31,7 +73,7 @@ class PlayerListener implements Listener {
         $location = $player->getLocation();
         $player->getWorld()->addSound($location, new XpLevelUpSound(30));
 
-        $player->setGamemode(GameMode::ADVENTURE);
+        $player->setGamemode(GameMode::SURVIVAL);
         $player->setHealth($player->getMaxHealth());
         $player->getHungerManager()->setFood(20);
 
@@ -74,5 +116,42 @@ class PlayerListener implements Listener {
 
             }
 
+    }
+
+    public function onBlockBreak(BlockBreakEvent $breakEvent) {
+        $player = $breakEvent->getPlayer();
+        $breakEvent->cancel();
+        $player->sendMessage(TextFormat::RED . "You can't break blocks here!");
+    }
+
+    public function onBlockPlace(BlockPlaceEvent $blockPlaceEvent) {
+        $player = $blockPlaceEvent->getPlayer();
+        $blockPlaceEvent->cancel();
+        $player->sendMessage(TextFormat::RED . "You can't place blocks here!");
+    }
+
+    public function onDamage(EntityDamageEvent $damageEvent) {
+        $entity = $damageEvent->getEntity();
+        $damageEvent->cancel();
+    }
+
+    public function onDrop(PlayerDropItemEvent $dropItemEvent) {
+        $player = $dropItemEvent->getPlayer();
+        $dropItemEvent->cancel();
+    }
+
+    public function onFallDamage(EntityDamageEvent $damageEvent) {
+        $entity = $damageEvent->getEntity();
+        $cause = $damageEvent->getCause();
+        if($cause == $damageEvent::CAUSE_FALL) {
+            $damageEvent->cancel();
+        }
+    }
+
+    public function onPVP(EntityDamageByEntityEvent $event) {
+        $damager = $event->getDamager();
+        $entity = $event->getEntity();
+
+        if (!$damager instanceof Player || !$entity instanceof Player) $event->cancel();
     }
 }
